91f80103bdd031f1ffb25d21cdb5d19512de6a45
wish911wish <wish911wish@gmail.com>
2022-08-28 18:22:39 +0900

    make dummy page

---
+++ src/components/pages/InvoicesPage.tsx
@@ -0,0 +1,10 @@
+/** @jsxImportSource @emotion/react */
+import HomeTemplate from '../templates/HomeTemplate'
+
+const ExpensesPage = () => {
+  return (
+    <HomeTemplate/>
+  );
+}
+
+export default ExpensesPage;
