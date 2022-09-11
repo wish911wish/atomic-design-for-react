ffcbec41f4ae3e7e97be37f8cde4ad1a9a6292eb
wish911wish <wish911wish@gmail.com>
2022-09-11 11:01:26 +0900

    make style file for font weight

---
+++ src/styles/font.ts
@@ -0,0 +1,16 @@
+export const size = {
+    xxs: 'none',
+    xs: '0.6rem',
+    s: '0.8rem',
+    m: '1rem',
+    l: '1.2rem',
+    xl: '1.4rem',
+    xxl: '1.6rem',
+    xxxl: '1.8rem',
+    xxxxl: '2.0rem',
+}
+
+export const weight = {
+    default: 400,
+    bold: 700,
+}
