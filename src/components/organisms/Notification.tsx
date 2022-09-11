bf3aa5a257c02c1d18565602cc62dbeb5b34576b
wish911wish <wish911wish@gmail.com>
2022-09-11 19:52:36 +0900

    fix notification.tsx

--- src/components/organisms/Notification.tsx
+++ src/components/organisms/Notification.tsx
@@ -1,9 +1,15 @@
 /** @jsxImportSource @emotion/react */
 
+import Txt, { InfoTxt } from '../atoms/Txt'
+import Time from '../atoms/Time'
+
 const Notification = () => {
   return (
     <div>
-    'ssss'
+      <Txt>Txt component</Txt>
+      <InfoTxt size='s'>InfoTxt component</InfoTxt>
+      <InfoTxt size='l'>InfoTxt l component</InfoTxt>
+      <Time>{(new Date()).getTime()}</Time>
     </div>
   );
 }
