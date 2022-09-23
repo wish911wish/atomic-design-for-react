36e0f0ee1dbec298d192e560675d28cf7e0334bf
wish911wish <wish911wish@gmail.com>
2022-09-23 21:34:25 +0900

    fix login form

--- src/components/organisms/LoginForm/index.tsx
+++ src/components/organisms/LoginForm/index.tsx
@@ -1,10 +1,18 @@
 /** @jsxImportSource @emotion/react */
-// import TextBox from '../../atoms/TextBox'
-// import { ComponentPropsWithoutRef } from "react"
 import { PrimaryButton } from '../../atoms/Button';
 import TextBoxWithTitle from '../../molecules/TextBoxWithTitle';
-// import { useId } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import styles from './styles'
+import { z } from "zod";
+
+export const LoginInputSchema = z.object({
+  password: z.string().min(1, "パスワードを入力してください"),
+  email: z
+    .string()
+    .min(1, "メールアドレスを入力してください")
+    .email("不正なメールアドレス形式です"),
+});
 
 type Values = {
   email: string;
@@ -16,29 +24,40 @@ type Props = {
   onValid: (values: Values) => Promise<void>;
 };
 
-// const defaultValues: Values = {
-//   email: "",
-//   password: "",
-// };
+const defaultValues: Values = {
+  email: "",
+  password: "",
+};
 
 const LoginForm = ({ initialValues, onValid }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { ...defaultValues, ...initialValues },
+    resolver: zodResolver(LoginInputSchema),
+  });
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid)}>
       <TextBoxWithTitle
         css={styles.textbox}
-        labelProps={{ children: "ユーザー名" }}
+        labelProps={{ children: "パスワード" }}
         textboxProps={{
-          placeholder: "田中 太郎",
+          ...register('password'),
+          placeholder: '',
         }}
-        error={'errors.name?.message'}
+        error={errors.password?.message}
       />
       <TextBoxWithTitle
         css={styles.textbox}
         labelProps={{ children: "メールアドレス" }}
         textboxProps={{
-          placeholder: "example@gmail.com",
+          ...register('email'),
+          placeholder: 'example@gmail.com',
         }}
-        error={'errors.email?.message'}
+        error={errors.email?.message}
       />
 
       <PrimaryButton css={styles.button}>LOG IN</PrimaryButton>
