eec44f5657315591cd467a8b8d2f51f5c5045538
wish911wish <wish911wish@gmail.com>
2022-09-24 11:45:40 +0900

    fix login form organisms

--- src/components/organisms/LoginForm/index.tsx
+++ src/components/organisms/LoginForm/index.tsx
@@ -1,18 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import { PrimaryButton } from '../../atoms/Button';
 import TextBoxWithTitle from '../../molecules/TextBoxWithTitle';
+import { LoginInputSchema } from '../../../services/api/login/schema';
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import styles from './styles'
-import { z } from "zod";
-
-export const LoginInputSchema = z.object({
-  password: z.string().min(1, "パスワードを入力してください"),
-  email: z
-    .string()
-    .min(1, "メールアドレスを入力してください")
-    .email("不正なメールアドレス形式です"),
-});
 
 type Values = {
   email: string;
